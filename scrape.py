3ec91f7de486aea2ad279517edfb6a057e1e1b2c
Field Notes Radar <99440089+fieldnotesradar@users.noreply.github.com>
2022-02-17 12:03:25 -0500

    comprehensive catalog search

--- scrape.py
+++ scrape.py
@@ -1,15 +1,23 @@
 from scrapy.spiders import SitemapSpider
+from scrapy.http import Request
 
 class MySpider(SitemapSpider):
     name = "sitemap"
-    sitemap_urls = [
-        'https://fieldnotesbrand.com/sitemaps-1-product-seasonalEditions-1-sitemap.xml',
-        'https://fieldnotesbrand.com/sitemaps-1-product-memoBooks-1-sitemap.xml',
-        'https://fieldnotesbrand.com/sitemaps-1-product-otherNotebooks-1-sitemap.xml'
+    sitemap_urls = ['https://fieldnotesbrand.com/robots.txt']
+    sitemap_rules = [
+        ('/products/', 'parse'),
     ]
+
+    other_urls = ['https://fieldnotesbrand.com/products/leap-of-faith']
+
+    def start_requests(self):
+        requests = list(super(MySpider, self).start_requests())
+        requests += [Request(x, self.parse) for x in self.other_urls]
+        return requests
 
     def parse(self, response):
         yield {
+            'id': response.xpath('//meta[@property="product:retailer_item_id"]/@content').get(),
             'item': response.xpath('//*[@class="l-panel-header__left"]/span/text()').get(),
             'url': response.url,
             'title': response.xpath('//*[@class="panel__title"]/text()').get().strip(),
@@ -17,6 +25,5 @@ class MySpider(SitemapSpider):
             'subtitle': response.xpath('//*[@class="panel__sub-title"][1]/text()').get().strip(),
             'details': response.xpath('//*[@class="panel__sub-title"][2]/text()').get().strip(),
             'price': response.xpath('//meta[@property="product:price:amount"]/@content').get(),
-            'id': response.xpath('//meta[@property="product:retailer_item_id"]/@content').get(),
             'availability': response.xpath('//meta[@property="product:availability"]/@content').get(),
         }
