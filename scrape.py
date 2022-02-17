f93957a0a0aa95124ddc31ce72cce5c865cc802d
Field Notes Radar <99440089+fieldnotesradar@users.noreply.github.com>
2022-02-16 21:53:10 -0500

    improved scraping

--- scrape.py
+++ scrape.py
@@ -3,14 +3,20 @@ from scrapy.spiders import SitemapSpider
 class MySpider(SitemapSpider):
     name = "sitemap"
     sitemap_urls = [
-    'https://fieldnotesbrand.com/sitemaps-1-product-seasonalEditions-1-sitemap.xml',
-    'https://fieldnotesbrand.com/sitemaps-1-product-memoBooks-1-sitemap.xml',
-    'https://fieldnotesbrand.com/sitemaps-1-product-otherNotebooks-1-sitemap.xml'
+        'https://fieldnotesbrand.com/sitemaps-1-product-seasonalEditions-1-sitemap.xml',
+        'https://fieldnotesbrand.com/sitemaps-1-product-memoBooks-1-sitemap.xml',
+        'https://fieldnotesbrand.com/sitemaps-1-product-otherNotebooks-1-sitemap.xml'
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-1]
-        filename = f'products/{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        yield {
+            'item': response.xpath('//*[@class="l-panel-header__left"]/span/text()').get(),
+            'url': response.url,
+            'name': response.xpath('//*[@class="panel__title"]/text()').get().strip(),
+            'details': response.xpath('//*[@class="panel__details"]/span/text()').get(),
+            'packaging': response.xpath('//*[@class="panel__sub-title"][1]/text()').get().strip(),
+            'dimensions': response.xpath('//*[@class="panel__sub-title"][2]/text()').get().strip(),
+            'price': response.xpath('//meta[@property="product:price:amount"]/@content').get(),
+            'id': response.xpath('//meta[@property="product:retailer_item_id"]/@content').get(),
+            'availability': response.xpath('//meta[@property="product:availability"]/@content').get(),
+        }
