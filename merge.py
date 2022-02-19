14f14b5269675835449f585c3b76ea7cb6818c35
Field Notes Radar <99440089+fieldnotesradar@users.noreply.github.com>
2022-02-19 18:16:50 -0500

    inital manual run to populate results.json

---
+++ merge.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+import json
+import arrow
+
+# Setup
+with open("updates.json", "r") as jsonFile:
+    updates = json.load(jsonFile)
+with open("results.json", "r") as jsonFile:
+    results = json.load(jsonFile)
+
+# Iterate over updates
+for update in updates:
+    for url, data in update.items():
+        # Match found
+        if url in results:
+            # Compare data between updates and results
+            changes = {}
+            for key, value in data.items():
+                update_value = value
+                result_value = results[url]['data'][key]
+                if update_value != result_value:
+                    changes[key] = {'old': result_value, 'new': update_value}
+            if changes:
+                results[url]['changelog'].append({
+                    'timestamp': arrow.utcnow(),
+                    'changes': changes
+                })
+                results[url]['data'] = data
+        # No match found
+        else:
+            # Build changelog
+            changes = {}
+            for key, value in data.items():
+                changes[key] = {'old': None, 'new': value}
+
+            # Append new result
+            results[url] = {
+                'data': data,
+                'changelog': [
+                    {
+                        'timestamp': arrow.utcnow(),
+                        'changes': changes
+                    }
+                ]
+            }
+
+
+with open("results.json", "w") as jsonFile:
+    json.dump(results, jsonFile, indent=4, sort_keys=True, default=str)
--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 from scrapy.spiders import SitemapSpider
 from scrapy.http import Request
+from datetime import timedelta
+import arrow
 
 class MySpider(SitemapSpider):
     name = "sitemap"
@@ -9,7 +11,7 @@ class MySpider(SitemapSpider):
         ('/films/', 'parse_film'),
         ('/dispatches/', 'parse_dispatch'),
     ]
-
+    
     other_product_urls = ['https://fieldnotesbrand.com/products/leap-of-faith']
 
     def start_requests(self):
@@ -17,35 +19,44 @@ class MySpider(SitemapSpider):
         requests += [Request(x, self.parse_product) for x in self.other_product_urls]
         return requests
 
+    def sitemap_filter(self, entries):
+        for entry in entries:
+            if self.minutes:
+                lastmod = arrow.get(entry['lastmod']).to('utc')
+                if (arrow.utcnow() - lastmod) <= timedelta(minutes=int(self.minutes)):
+                    yield entry
+            else:
+                yield entry
+
     def parse_product(self, response):
         yield {
-            'url': response.url,
-            'type': 'product',
-            'id': response.xpath('//meta[@property="product:retailer_item_id"]/@content').get(),
-            'item': response.xpath('//*[@class="l-panel-header__left"]/span/text()').get(),
-            'title': response.xpath('//*[@class="panel__title"]/text()').get().strip(),
-            'dimensions': response.xpath('//*[@class="panel__details"]/span/text()').get(),
-            'subtitle': response.xpath('//*[@class="panel__sub-title"][1]/text()').get().strip(),
-            'details': response.xpath('//*[@class="panel__sub-title"][2]/text()').get().strip(),
-            'price': response.xpath('//meta[@property="product:price:amount"]/@content').get(),
-            'availability': response.xpath('//meta[@property="product:availability"]/@content').get(),
+            response.url: {
+                'type': 'product',
+                'id': response.xpath('//meta[@property="product:retailer_item_id"]/@content').get(),
+                'item': response.xpath('//*[@class="l-panel-header__left"]/span/text()').get(),
+                'title': response.xpath('//*[@class="panel__title"]/text()').get().strip(),
+                'price': response.xpath('//meta[@property="product:price:amount"]/@content').get(),
+                'availability': response.xpath('//meta[@property="product:availability"]/@content').get(),
+            }
         }
     
     def parse_film(self, response):
         yield {
-            'url': response.url,
-            'type': 'film',
-            'title': response.xpath('//meta[@property="og:title"]/@content').get(),
-            'video_url': response.xpath('//*[@class="film-teaser__text"]/a/@href').get(),
-            'teaser_link': response.xpath('//*[@class="film-teaser__link"]/@href').get(),
-            'teaser_text': response.xpath('//*[@class="film-teaser__text"]/p/text()').get(),
+            response.url: {
+                'type': 'film',
+                'title': response.xpath('//meta[@property="og:title"]/@content').get(),
+                'video_url': response.xpath('//*[@class="film-teaser__text"]/a/@href').get(),
+                'teaser_link': response.xpath('//*[@class="film-teaser__link"]/@href').get(),
+                'teaser_text': response.xpath('//*[@class="film-teaser__text"]/p/text()').get(),
+            }
         }
 
     def parse_dispatch(self, response):
         yield {
-            'url': response.url,
-            'type': 'dispatch',
-            'category': response.xpath('//*[@class="blog-post__header"]/h2/a/text()').get(),
-            'title': response.xpath('//*[@class="blog-post__header"]/h1/text()').get(),
-            'date': response.xpath('//*[@class="blog-post__header"]/h3/text()').get(),
+            response.url: {
+                'type': 'dispatch',
+                'category': response.xpath('//*[@class="blog-post__header"]/h2/a/text()').get(),
+                'title': response.xpath('//*[@class="blog-post__header"]/h1/text()').get(),
+                'date': response.xpath('//*[@class="blog-post__header"]/h3/text()').get(),
+            }
         }
